95a110e18ce0317d5b61deec64d92eec6dd6f8fb
Jed Watson <jed.watson@me.com>
2015-05-24 20:52:00 +1000

    Updating Build

--- example/src/example.js
+++ example/src/example.js
@@ -5,7 +5,7 @@ var React = require('react');
 var App = React.createClass({
 	getInitialState: function() {
 		return {
-			code: "# React Markdown Editor\n\n* A list\n\nSome **bold** and _italic_ text\n\n> A quote...\n\nBy [Jed Watson](https://github.com/JedWatson)"
+			code: "# React Markdown Editor\n\n* A list\n\nSome **bold** and _italic_ text\n\n> A quote...\n\nBy [Jed Watson](https://github.com/JedWatson) and [Joss Mackison](https://github.com/jossmac)"
 		};
 	},
 	updateCode: function(newCode) {
