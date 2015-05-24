5a2ff46a51ca8a1aa2f1ab93c08be85010230401
Jed Watson <jed.watson@me.com>
2015-05-24 20:56:52 +1000

    Update 2015-05-24T10:56:42.084Z

--- src/icons/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	bold:   require('./bold'),
-	italic: require('./italic'),
-	link:   require('./link'),
-	uList:  require('./uList'),
-	oList:  require('./oList'),
-	quote:  require('./quote'),
-};
