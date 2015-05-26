89caf817015ac5bd394e2f21edc38a299de436dd
Jed Watson <jed.watson@me.com>
2015-05-27 09:40:11 +1000

    Updating build

--- dist/react-md-editor.js
+++ dist/react-md-editor.js
@@ -2,7 +2,13 @@
 (function (global){
 'use strict';
 
+Object.defineProperty(exports, '__esModule', {
+	value: true
+});
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _formatJs = require('./format.js');
 
 var classNames = require('classnames');
 var CM = require('codemirror');
@@ -12,104 +18,6 @@ var Icons = require('./icons');
 require('codemirror/mode/xml/xml');
 require('codemirror/mode/markdown/markdown');
 require('codemirror/addon/edit/continuelist');
-
-var FORMATS = {
-	h1: { before: '# ' },
-	h2: { before: '## ' },
-	h3: { before: '### ' },
-	bold: { before: '**', after: '**' },
-	italic: { before: '_', after: '_' },
-	quote: { before: '> ' },
-	oList: { before: '1. ' },
-	uList: { before: '* ' }
-};
-
-function getCursorState(cm, pos) {
-	pos = pos || cm.getCursor('start');
-	var cs = {};
-	var token = cs.token = cm.getTokenAt(pos);
-	if (!token.type) return cs;
-	var tokens = token.type.split(' ');
-	tokens.forEach(function (t) {
-		switch (t) {
-			case 'header-1':
-				cs.h1 = true;
-				break;
-			case 'header-2':
-				cs.h2 = true;
-				break;
-			case 'header-3':
-				cs.h3 = true;
-				break;
-			case 'strong':
-				cs.bold = true;
-				break;
-			case 'quote':
-				cs.quote = true;
-				break;
-			case 'em':
-				cs.italic = true;
-				break;
-			case 'link':
-				cs.link = true;
-				cs.link_label = true;
-				break;
-			case 'string':
-				cs.link = true;
-				cs.link_href = true;
-				break;
-			case 'variable-2':
-				var text = cm.getLine(pos.line);
-				if (/^\s*\d+\.\s/.test(text)) {
-					cs.oList = true;
-				} else {
-					cs.uList = true;
-				}
-				break;
-		}
-	});
-	return cs;
-}
-
-function toggleBlock(cm, key) {
-	var cs = getCursorState(cm);
-
-	var insertBefore = FORMATS[key].before;
-	var insertAfter = FORMATS[key].after || '';
-
-	var startPoint = cm.getCursor('start');
-	var endPoint = cm.getCursor('end');
-
-	if (cs[key]) {
-		var line = cm.getLine(startPoint.line);
-		var startPos = startPoint.ch;
-		while (startPos) {
-			if (line.substr(startPos, insertBefore.length) === insertBefore) {
-				break;
-			}
-			startPos--;
-		}
-		var endPos = endPoint.ch;
-		while (endPos <= line.length) {
-			if (line.substr(endPos, insertAfter.length) === insertAfter) {
-				break;
-			}
-			endPos++;
-		}
-		var start = line.slice(0, startPos);
-		var mid = line.slice(startPos + insertBefore.length, endPos);
-		var end = line.slice(endPos + insertAfter.length);
-		cm.replaceRange(start + mid + end, { line: startPoint.line, ch: 0 }, { line: startPoint.line, ch: line.length + 1 });
-		startPoint.ch -= insertBefore.length;
-		endPoint.ch -= insertAfter.length;
-	} else {
-		cm.replaceSelection(insertBefore + cm.getSelection() + insertAfter);
-		startPoint.ch += insertBefore.length;
-		endPoint.ch += insertAfter.length;
-	}
-	cm.setSelection(startPoint, endPoint);
-	cm.focus();
-}
 
 var MarkdownEditor = React.createClass({
 	displayName: 'MarkdownEditor',
@@ -169,7 +77,7 @@ var MarkdownEditor = React.createClass({
 	},
 
 	updateCursorState: function updateCursorState() {
-		this.setState({ cs: getCursorState(this.codeMirror) });
+		this.setState({ cs: (0, _formatJs.getCursorState)(this.codeMirror) });
 	},
 
 	codemirrorValueChanged: function codemirrorValueChanged(doc, change) {
@@ -178,32 +86,27 @@ var MarkdownEditor = React.createClass({
 		this.props.onChange && this.props.onChange(newValue);
 	},
 
-	fieldValueChanged: function fieldValueChanged(e) {},
-
-	toggle: function toggle(csKey) {
-		if (FORMATS[csKey]) {
-			toggleBlock(this.codeMirror, csKey);
-		}
+	toggleFormat: function toggleFormat(formatKey) {
+		(0, _formatJs.applyFormat)(this.codeMirror, formatKey);
 	},
 
 	renderIcon: function renderIcon(icon) {
 		return React.createElement('span', { dangerouslySetInnerHTML: { __html: icon }, className: 'MDEditor_toolbarButton_icon' });
 	},
 
-	renderButton: function renderButton(csKey, label, action) {
-		if (!action) action = this.toggle.bind(this, csKey);
+	renderButton: function renderButton(formatKey, label, action) {
+		if (!action) action = this.toggleFormat.bind(this, formatKey);
 
-		var isTextIcon = csKey == 'h1' || csKey == 'h2' || csKey == 'h3';
+		var isTextIcon = formatKey === 'h1' || formatKey === 'h2' || formatKey === 'h3';
 		var className = classNames('MDEditor_toolbarButton', {
-			'MDEditor_toolbarButton--pressed': this.state.cs[csKey]
-		}, 'MDEditor_toolbarButton--' + csKey);
+			'MDEditor_toolbarButton--pressed': this.state.cs[formatKey] }, 'MDEditor_toolbarButton--' + formatKey);
 
 		var labelClass = isTextIcon ? 'MDEditor_toolbarButton_label-icon' : 'MDEditor_toolbarButton_label';
 
 		return React.createElement(
 			'button',
-			{ className: className, onClick: action, title: csKey },
-			isTextIcon ? null : this.renderIcon(Icons[csKey]),
+			{ className: className, onClick: action, title: formatKey },
+			isTextIcon ? null : this.renderIcon(Icons[formatKey]),
 			React.createElement(
 				'span',
 				{ className: labelClass },
@@ -223,16 +126,12 @@ var MarkdownEditor = React.createClass({
 			this.renderButton('italic', 'i'),
 			this.renderButton('oList', 'ol'),
 			this.renderButton('uList', 'ul'),
-			this.renderButton('quote', 'q'),
-			this.renderButton('link', 'a')
+			this.renderButton('quote', 'q')
 		);
 	},
 
 	render: function render() {
-		var editorClassName = 'MDEditor_editor';
-		if (this.state.isFocused) {
-			editorClassName += ' MDEditor_editor--focused';
-		}
+		var editorClassName = classNames('MDEditor_editor', { 'MDEditor_editor--focused': this.state.isFocused });
 		return React.createElement(
 			'div',
 			{ className: 'MDEditor' },
@@ -240,19 +139,19 @@ var MarkdownEditor = React.createClass({
 			React.createElement(
 				'div',
 				{ className: editorClassName },
-				React.createElement('textarea', { ref: 'codemirror', name: this.props.path, value: this.props.value, onChange: this.fieldValueChanged, autoComplete: 'off' })
+				React.createElement('textarea', { ref: 'codemirror', name: this.props.path, defaultValue: this.props.value, autoComplete: 'off' })
 			)
 		);
 	}
 
 });
 
-module.exports = MarkdownEditor;
-
-// console.log("field changed");
+exports['default'] = MarkdownEditor;
+module.exports = exports['default'];
+/*this.renderButton('link', 'a')*/
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./icons":8,"classnames":undefined,"codemirror":3,"codemirror/addon/edit/continuelist":2,"codemirror/mode/markdown/markdown":4,"codemirror/mode/xml/xml":6}],2:[function(require,module,exports){
+},{"./format.js":7,"./icons":9,"classnames":undefined,"codemirror":3,"codemirror/addon/edit/continuelist":2,"codemirror/mode/markdown/markdown":4,"codemirror/mode/xml/xml":6}],2:[function(require,module,exports){
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
@@ -10393,9 +10292,131 @@ if (!CodeMirror.mimeModes.hasOwnProperty("text/html"))
 },{"../../lib/codemirror":3}],7:[function(require,module,exports){
 'use strict';
 
-module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M747 1521q74 32 140 32 376 0 376-335 0-114-41-180-27-44-61.5-74t-67.5-46.5-80.5-25-84-10.5-94.5-2q-73 0-101 10 0 53-.5 159t-.5 158q0 8-1 67.5t-.5 96.5 4.5 83.5 12 66.5zm-14-746q42 7 109 7 82 0 143-13t110-44.5 74.5-89.5 25.5-142q0-70-29-122.5t-79-82-108-43.5-124-14q-50 0-130 13 0 50 4 151t4 152q0 27-.5 80t-.5 79q0 46 1 69zm-541 889l2-94q15-4 85-16t106-27q7-12 12.5-27t8.5-33.5 5.5-32.5 3-37.5.5-34v-65.5q0-982-22-1025-4-8-22-14.5t-44.5-11-49.5-7-48.5-4.5-30.5-3l-4-83q98-2 340-11.5t373-9.5q23 0 68.5.5t67.5.5q70 0 136.5 13t128.5 42 108 71 74 104.5 28 137.5q0 52-16.5 95.5t-39 72-64.5 57.5-73 45-84 40q154 35 256.5 134t102.5 248q0 100-35 179.5t-93.5 130.5-138 85.5-163.5 48.5-176 14q-44 0-132-3t-132-3q-106 0-307 11t-231 12z"/></svg>';
+Object.defineProperty(exports, '__esModule', {
+	value: true
+});
+exports.getCursorState = getCursorState;
+exports.applyFormat = applyFormat;
+var FORMATS = {
+	h1: { type: 'block', token: 'header-1', before: '#', re: /^#\s+/, placeholder: 'Heading' },
+	h2: { type: 'block', token: 'header-2', before: '##', re: /^##\s+/, placeholder: 'Heading' },
+	h3: { type: 'block', token: 'header-3', before: '###', re: /^###\s+/, placeholder: 'Heading' },
+	bold: { type: 'inline', token: 'strong', before: '**', after: '**', placeholder: 'bold text' },
+	italic: { type: 'inline', token: 'em', before: '_', after: '_', placeholder: 'italic text' },
+	quote: { type: 'block', token: 'quote', re: /^\>\s+/, before: '>', placeholder: 'quote' },
+	oList: { type: 'block', before: '1. ', re: /^\d+\.\s+/, placeholder: 'List' },
+	uList: { type: 'block', before: '* ', re: /^[\*\-]\s+/, placeholder: 'List' }
+};
+
+var FORMAT_TOKENS = {};
+Object.keys(FORMATS).forEach(function (key) {
+	if (FORMATS[key].token) FORMAT_TOKENS[FORMATS[key].token] = key;
+});
+
+function getCursorState(cm, pos) {
+	pos = pos || cm.getCursor('start');
+	var cs = {};
+	var token = cs.token = cm.getTokenAt(pos);
+	if (!token.type) return cs;
+	var tokens = token.type.split(' ');
+	tokens.forEach(function (t) {
+		if (FORMAT_TOKENS[t]) {
+			cs[FORMAT_TOKENS[t]] = true;
+			return;
+		}
+		switch (t) {
+			case 'link':
+				cs.link = true;
+				cs.link_label = true;
+				break;
+			case 'string':
+				cs.link = true;
+				cs.link_href = true;
+				break;
+			case 'variable-2':
+				var text = cm.getLine(pos.line);
+				if (/^\s*\d+\.\s/.test(text)) {
+					cs.oList = true;
+				} else {
+					cs.uList = true;
+				}
+				break;
+		}
+	});
+	return cs;
+}
+
+function applyFormat(cm, key) {
+	var cs = getCursorState(cm);
+	var format = FORMATS[key];
+	operations[format.type + (cs[key] ? 'Remove' : 'Apply')](cm, format);
+}
+
+var operations = {
+	inlineApply: function inlineApply(cm, format) {
+		var startPoint = cm.getCursor('start');
+		var endPoint = cm.getCursor('end');
+
+		cm.replaceSelection(format.before + cm.getSelection() + format.after);
+
+		startPoint.ch += format.before.length;
+		endPoint.ch += format.after.length;
+		cm.setSelection(startPoint, endPoint);
+		cm.focus();
+	},
+	inlineRemove: function inlineRemove(cm, format) {
+		var startPoint = cm.getCursor('start');
+		var endPoint = cm.getCursor('end');
+		var line = cm.getLine(startPoint.line);
+
+		var startPos = startPoint.ch;
+		while (startPos) {
+			if (line.substr(startPos, format.before.length) === format.before) {
+				break;
+			}
+			startPos--;
+		}
+
+		var endPos = endPoint.ch;
+		while (endPos <= line.length) {
+			if (line.substr(endPos, format.after.length) === format.after) {
+				break;
+			}
+			endPos++;
+		}
+
+		var start = line.slice(0, startPos);
+		var mid = line.slice(startPos + format.before.length, endPos);
+		var end = line.slice(endPos + format.after.length);
+		cm.replaceRange(start + mid + end, { line: startPoint.line, ch: 0 }, { line: startPoint.line, ch: line.length + 1 });
+		cm.setSelection({ line: startPoint.line, ch: start.length }, { line: startPoint.line, ch: (start + mid).length });
+		cm.focus();
+	},
+	blockApply: function blockApply(cm, format) {
+		var startPoint = cm.getCursor('start');
+		var line = cm.getLine(startPoint.line);
+		var text = format.before + ' ' + (line.length ? line : format.placeholder);
+		cm.replaceRange(text, { line: startPoint.line, ch: 0 }, { line: startPoint.line, ch: line.length + 1 });
+		cm.setSelection({ line: startPoint.line, ch: format.before.length + 1 }, { line: startPoint.line, ch: text.length });
+		cm.focus();
+	},
+	blockRemove: function blockRemove(cm, format) {
+		var startPoint = cm.getCursor('start');
+		var line = cm.getLine(startPoint.line);
+		var text = line.replace(format.re, '');
+		console.log(text);
+		cm.replaceRange(text, { line: startPoint.line, ch: 0 }, { line: startPoint.line, ch: line.length + 1 });
+		cm.setSelection({ line: startPoint.line, ch: 0 }, { line: startPoint.line, ch: text.length });
+		cm.focus();
+	}
+};
 
 },{}],8:[function(require,module,exports){
+'use strict';
+
+module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M747 1521q74 32 140 32 376 0 376-335 0-114-41-180-27-44-61.5-74t-67.5-46.5-80.5-25-84-10.5-94.5-2q-73 0-101 10 0 53-.5 159t-.5 158q0 8-1 67.5t-.5 96.5 4.5 83.5 12 66.5zm-14-746q42 7 109 7 82 0 143-13t110-44.5 74.5-89.5 25.5-142q0-70-29-122.5t-79-82-108-43.5-124-14q-50 0-130 13 0 50 4 151t4 152q0 27-.5 80t-.5 79q0 46 1 69zm-541 889l2-94q15-4 85-16t106-27q7-12 12.5-27t8.5-33.5 5.5-32.5 3-37.5.5-34v-65.5q0-982-22-1025-4-8-22-14.5t-44.5-11-49.5-7-48.5-4.5-30.5-3l-4-83q98-2 340-11.5t373-9.5q23 0 68.5.5t67.5.5q70 0 136.5 13t128.5 42 108 71 74 104.5 28 137.5q0 52-16.5 95.5t-39 72-64.5 57.5-73 45-84 40q154 35 256.5 134t102.5 248q0 100-35 179.5t-93.5 130.5-138 85.5-163.5 48.5-176 14q-44 0-132-3t-132-3q-106 0-307 11t-231 12z"/></svg>';
+
+},{}],9:[function(require,module,exports){
 'use strict';
 
 module.exports = {
@@ -10406,27 +10427,27 @@ module.exports = {
 	oList: require('./oList'),
 	quote: require('./quote') };
 
-},{"./bold":7,"./italic":9,"./link":10,"./oList":11,"./quote":12,"./uList":13}],9:[function(require,module,exports){
+},{"./bold":8,"./italic":10,"./link":11,"./oList":12,"./quote":13,"./uList":14}],10:[function(require,module,exports){
 'use strict';
 
 module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M384 1662l17-85q6-2 81.5-21.5t111.5-37.5q28-35 41-101 1-7 62-289t114-543.5 52-296.5v-25q-24-13-54.5-18.5t-69.5-8-58-5.5l19-103q33 2 120 6.5t149.5 7 120.5 2.5q48 0 98.5-2.5t121-7 98.5-6.5q-5 39-19 89-30 10-101.5 28.5t-108.5 33.5q-8 19-14 42.5t-9 40-7.5 45.5-6.5 42q-27 148-87.5 419.5t-77.5 355.5q-2 9-13 58t-20 90-16 83.5-6 57.5l1 18q17 4 185 31-3 44-16 99-11 0-32.5 1.5t-32.5 1.5q-29 0-87-10t-86-10q-138-2-206-2-51 0-143 9t-121 11z"/></svg>';
 
-},{}],10:[function(require,module,exports){
+},{}],11:[function(require,module,exports){
 'use strict';
 
 module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z"/></svg>';
 
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 'use strict';
 
 module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M381 1620q0 80-54.5 126t-135.5 46q-106 0-172-66l57-88q49 45 106 45 29 0 50.5-14.5t21.5-42.5q0-64-105-56l-26-56q8-10 32.5-43.5t42.5-54 37-38.5v-1q-16 0-48.5 1t-48.5 1v53h-106v-152h333v88l-95 115q51 12 81 49t30 88zm2-627v159h-362q-6-36-6-54 0-51 23.5-93t56.5-68 66-47.5 56.5-43.5 23.5-45q0-25-14.5-38.5t-39.5-13.5q-46 0-81 58l-85-59q24-51 71.5-79.5t105.5-28.5q73 0 123 41.5t50 112.5q0 50-34 91.5t-75 64.5-75.5 50.5-35.5 52.5h127v-60h105zm1409 319v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-14 9-23t23-9h1216q13 0 22.5 9.5t9.5 22.5zm-1408-899v99h-335v-99h107q0-41 .5-122t.5-121v-12h-2q-8 17-50 54l-71-76 136-127h106v404h108zm1408 387v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-14 9-23t23-9h1216q13 0 22.5 9.5t9.5 22.5zm0-512v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5z"/></svg>';
 
-},{}],12:[function(require,module,exports){
+},{}],13:[function(require,module,exports){
 'use strict';
 
 module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M832 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136zm896 0v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136z"/></svg>';
 
-},{}],13:[function(require,module,exports){
+},{}],14:[function(require,module,exports){
 'use strict';
 
 module.exports = '<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M384 1408q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm0-512q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm1408 416v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5zm-1408-928q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm1408 416v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5zm0-512v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5z"/></svg>';
